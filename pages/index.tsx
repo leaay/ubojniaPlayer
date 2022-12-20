01b796e93ab83b9f6321419bba50fd06a7fd4927
leaay <wiktorgs98@gmail.com>
2022-12-20 15:01:10 +0100

    favico
    123

--- pages/index.tsx
+++ pages/index.tsx
@@ -148,7 +148,10 @@ const Home: NextPage = () => {
       <Head>
         <title>UBOJNIA PLAYER</title>
         <meta name="description" content="UBOJNIA PLAYER" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="favico/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="favico/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="favico/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
       </Head>
 
 
